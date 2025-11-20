9d3b826924d377de135315ee18dbc9fa3795f0a5
Claire Zarakas <czarakas@uw.edu>
2025-11-20 15:17:31 -0800

    Define color settings to be used across figures

---
+++ nbs/vis_settings.py
@@ -0,0 +1,16 @@
+
+flux_types = {
+    'net-effect':'#58BBCC',
+    'removal':'#519E3E',
+    'activity-emission':'#3B75AF',
+    'CDR+baseline':'#8D69B8'
+}
+
+lag_colors = {
+    'baseline': 'black',
+    'instant-removal-offsetting':'#F7DC9C', #'#D98C00',#
+    'physical-delay-offsetting':'#FE648D',
+    'counterfactual-delay-offsetting':'#EB6E6C',
+    'accelerated-emissions-offsetting':"#C23B22",
+    'upfront-embodied-offsetting': '#F27234'
+}
