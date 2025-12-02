4f1bc97077ae63777c8f96f8b03146f8e201981d
Claire Zarakas <czarakas@uw.edu>
2025-12-02 11:40:53 -0800

    Update figures 3 and 4

--- nbs/vis_settings.py
+++ nbs/vis_settings.py
@@ -1,5 +1,12 @@
+panel_titles = [
+    "A) Physical delay",
+    "B) Counterfactual delay",
+    "C) Accelerated emissions",
+    "D) Upfront embodied emissions",
+]
 
 flux_types = {
+    'baseline':'black',
     'net-effect':'#58BBCC',
     'removal':'#519E3E',
     'activity-emission':'#3B75AF',
@@ -13,4 +20,12 @@ lag_colors = {
     'counterfactual-delay-offsetting':'#EB6E6C',
     'accelerated-emissions-offsetting':"#C23B22",
     'upfront-embodied-offsetting': '#F27234'
+}
+
+line_labels = {
+    'baseline': 'Fossil emissions to neutralize',
+    'net-effect':'Net CDR activity',
+    'removal':'Removal',
+    'activity-emission':'CDR activity emissions',
+    'CDR+baseline':'CDR + neutralized fossil emissions'
 }
